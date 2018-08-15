29b1bfcb897726716a35fdf3166d5efcb2c1f22e
Serhii A. Miroshnychenko <s.a.miroshnychenko@globallogic.com>
2018-08-15 18:09:36 +0300

    if to switch update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,13 +3,29 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int value = 1;
+        int value = 3;
         if (value == 1) {
             System.out.println("Value is 1");
         } else if (value == 2) {
             System.out.println("Value is 2");
-        }else {
+        } else {
             System.out.println("Nor 1 or 2");
         }
+
+        int switchValue = 1;
+
+        switch (switchValue) {
+            case 1:
+                System.out.println("Value is 1");
+                break;
+            case 2:
+                System.out.println("Value is 2");
+                break;
+            default:
+                System.out.println("Value is not 1 or 2 ");
+                break;
+        }
+
+
     }
 }
