e8b64c53678e2a31c2b67ae2da5ddcdf70c6a147
Serhii A. Miroshnychenko <s.a.miroshnychenko@globallogic.com>
2018-08-15 17:49:38 +0300

    if to switch

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int value = 1;
+        if (value == 1) {
+            System.out.println("Value is 1");
+        } else if (value == 2) {
+            System.out.println("Value is 2");
+        }else {
+            System.out.println("Nor 1 or 2");
+        }
+    }
+}
